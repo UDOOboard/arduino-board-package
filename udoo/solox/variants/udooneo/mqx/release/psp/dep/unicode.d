c7209004e51b08bd8bada052d87ddd28969a6b0e
ektor5 <ek5.chimenti@gmail.com>
2016-02-18 17:26:40 +0100

    Remove compilation object and linking files

--- udoo/solox/variants/udooneo/mqx/release/psp/dep/unicode.d
+++
@@ -1,8 +0,0 @@
-/home/francesco/Freescale/Freescale_MQX_4_1_IMX6SX/lib/imx6sx_sdb_m4.gcc_arm/release/psp/obj/unicode.o: \
- /home/francesco/Freescale/Freescale_MQX_4_1_IMX6SX/mqx/source/string/unicode.c \
- /usr/local/gcc-arm-none-eabi-4_9-2014q4/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_9-2014q4/arm-none-eabi/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_9-2014q4/arm-none-eabi/include/machine/_default_types.h \
- /usr/local/gcc-arm-none-eabi-4_9-2014q4/arm-none-eabi/include/sys/features.h \
- /usr/local/gcc-arm-none-eabi-4_9-2014q4/arm-none-eabi/include/sys/_intsup.h \
- /home/francesco/Freescale/Freescale_MQX_4_1_IMX6SX/mqx/source/include/unicode.h
